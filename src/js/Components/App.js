686f71ff50c58ddf995ccdcd65aab0324119bcb8
ninjabear <nino.ostir@gmail.com>
2025-08-12 22:51:57 +0200

    App.js has been created as well as index.js and main.scss stylesheet

---
+++ src/js/Components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../../styles/main.scss";
+
+const App = () => {
+    return (
+        <>
+            <p>Color Mixer Tool</p>
+        </>
+    )
+}
+
+export default App;
