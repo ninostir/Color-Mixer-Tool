93bf1320b3fae1b425394d9fe9158e2ef10b0730
ninjabear <nino.ostir@gmail.com>
2025-08-25 18:47:01 +0200

    added the function to pick the color in ColorBlock component

--- src/js/Components/MixerTool/ColorBlock.js
+++ src/js/Components/MixerTool/ColorBlock.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import styles from "./ColorBlock.scss";
 
 const ColorBlock = ({ index }) => {
+
+    const inputRef = useRef(null);
+    const [color, setColor] = useState("#FFFFFF");
+
+    const handleDivClick = () => {
+        inputRef.current.click();
+    };
+
+    const handleColorChange = (e) => {
+        setColor(e.target.value);
+    };
+
     return (
         <>
-            <div className={`color--mixer__color${index} color` }></div>
+            <div className={`color--mixer__color${index} color` }style={{
+                backgroundColor: color,
+            }}
+                 onClick={handleDivClick}
+            ></div>
+
+            <input
+                type="color"
+                ref={inputRef}
+                value={color}
+                onChange={handleColorChange}
+                style={{ display: "none" }}
+            />
+
         </>
     )
 }
